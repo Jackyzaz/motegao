518b98e701ed5aecb98f7e4f0d35bee0a619797e
Soravit <soravit.sukkarn@gmail.com>
2026-02-05 15:40:59 +0700

    chore: update nginx allow docs

--- motegao/api/core/settings/production.py
+++ motegao/api/core/settings/production.py
@@ -4,5 +4,11 @@ from motegao.api.core.settings.app import AppSettings
 class ProdAppSettings(AppSettings):
     MONGODB_HOST: str = "motegao-mongodb"
 
+    # Enable API docs in production
+    # Set these to None to disable docs
+    DOCS_URL: str = "/docs"
+    REDOC_URL: str = "/redoc"
+    OPENAPI_URL: str = "/openapi.json"
+
     class Config(AppSettings.Config):
         env_file = "prod.env"
